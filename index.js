54c7e3698c396a992ae4c64322862c7a31886148
Ben Kogan <benkogan@me.com>
2014-08-31 16:09:23 -0400

    Add `moveSync` fn, rename `traverse` to `traverseSync`

--- index.js
+++ index.js
@@ -3,16 +3,17 @@
  */
 
 var fs = require('fs');
+var format = require('util').format;
 var resolve = require('path').resolve;
-
-// TODO: if output dir not exist, make it (recursively)
+var dirname = require('path').dirname;
+var basename = require('path').basename;
 
 /**
  * traverse dirs and apply second arg to contents
  * applies fn to path of item
  */
 
-function traverse(startPath, fn) {
+function traverseSync(startPath, fn) {
   var i, item, isDir;
   var contents = fs.readdirSync(startPath);
 
@@ -20,12 +21,34 @@ function traverse(startPath, fn) {
     item = resolve(startPath, contents[i]);
     isDir = fs.lstatSync(item).isDirectory();
 
-    if (isDir) traverse(item, fn);
+    if (isDir) traverseSync(item, fn);
     else fn(item);
+  }
+}
+
+/*
+ * file should be path to file
+ * deals with name conflicts
+ */
+
+function moveSync(file, newPath) {
+  var siblings = fs.readdirSync(newPath);
+
+  _moveSync(0);
+
+  function _moveSync(i) {
+    var path = (i) ? format('%s-%d', file, i) : file;
+    var name = basename(path);
+    var unique = (siblings.indexOf(name) == -1);
+    var dest = resolve(newPath, name);
+
+    if (unique) fs.renameSync(file, dest);
+    else _moveSync(++i);
   }
 }
 
 //---- Exports
 
-module.exports.traverse = traverse;
+module.exports.traverseSync = traverseSync;
+module.exports.moveSync = moveSync;
 
--- test/test.js
+++ test/test.js
@@ -12,7 +12,6 @@ var lib = require('./lib');
 var testDir = resolve(__dirname, '..', '.test_dir');
 
 describe('flatten', function(){
-
   before(function(){
     if (!fs.existsSync(testDir)) {
       fs.mkdirSync(testDir);
@@ -21,17 +20,15 @@ describe('flatten', function(){
     } else throw new Error('Test directory aleady exists');
   })
 
-  after(function(){
-    rm.sync(testDir);
-  })
+  after(function(){ rm.sync(testDir); })
 
-  describe('#traverse(path, fn)', function(){
+  describe('#traverseSync(path, fn)', function(){
     it('should map fn over an empty dir structure', function(){
       var dirs = [];
       var dir = resolve(testDir, 'base');
       lib.build(dir, 'base case');
 
-      flatten.traverse(dir, function(content){
+      flatten.traverseSync(dir, function(content){
         dirs.push(content);
       });
 
@@ -43,16 +40,51 @@ describe('flatten', function(){
     it('should map fn over a 3-level dir structure', function(){ testLvl(3); })
   })
 
+  describe('#moveSync(file, path)', function(){
+    it('should move a file', function(){
+      var parentDir = resolve(testDir, 'move');
+      var childDir = resolve(parentDir, 'dest');
+      var file = resolve(parentDir, 'file');
+
+      fs.mkdirSync(parentDir);
+      fs.mkdirSync(childDir);
+      fs.writeFileSync(file);
+
+      flatten.moveSync(file, childDir);
+      fs.readdirSync(parentDir).should.eql(['dest']);
+      fs.readdirSync(childDir).should.eql(['file']);
+    })
+    it('should move and rename a file if conflicts', function(){
+      var parentDir = resolve(testDir, 'move_2');
+      var childDir = resolve(parentDir, 'dest');
+      var file = resolve(parentDir, 'file');
+      var conflict0 = resolve(childDir, 'file');
+      var conflict1 = resolve(childDir, 'file-1');
+      var conflict2 = resolve(childDir, 'file-2');
+
+      fs.mkdirSync(parentDir);
+      fs.mkdirSync(childDir);
+      fs.writeFileSync(file);
+      fs.writeFileSync(conflict0);
+      fs.writeFileSync(conflict1);
+      fs.writeFileSync(conflict2);
+
+      flatten.moveSync(file, childDir);
+      fs.readdirSync(parentDir).should.eql(['dest']);
+      fs.readdirSync(childDir).should.eql(['file', 'file-1', 'file-2', 'file-3']);
+    })
+  })
 }) // end `describe('flatten')`
 
 function testLvl(level) {
-  var dirs = [];
   var dir = resolve(testDir, 'level_' + level);
   var contents = lib.build(dir, level);
+  var mapped = [];
 
-  flatten.traverse(dir, function(content){
-    dirs.push(content);
+  flatten.traverseSync(dir, function(content){
+    mapped.push(content);
   });
 
-  dirs.sort().should.eql(contents.sort());
+  mapped.sort().should.eql(contents.sort());
 }
+
